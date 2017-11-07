aa34540c3ae52bd6e2c7b19c464077c63b7cb63a
Kan Li <likan999@users.noreply.github.com>
2017-11-07 10:22:12 -0800

    Fix header files for ncurses library.
    Summary:

--- ncurses/build-install.bash
+++ ncurses/build-install.bash
@@ -1,47 +1,60 @@
 #!/usr/bin/env bash
 
 args=(
-	--without-shared
-	--without-debug 
-	--without-profile
-	--without-cxx-shared
-	--without-ada
-	--disable-db-install
-	--without-manpages
-	--without-curses-h
-	--enable-pc-files
-	--with-termlib=tinfo
-	--with-ticlib=tic
-	--with-sysmouse
-	--enable-overwrite
-	--with-xterm-kbs=DEL
-	--enable-sp-funcs
-	--enable-const
-	--enable-ext-colors
-	--enable-ext-mouse
-	--enable-ext-putwin
-	--enable-no-padding
-	--enable-signed-char
-	--enable-sigwinch
-	--enable-tcap-names
+  --without-shared
+  --without-debug 
+  --without-profile
+  --with-pkg-config-libdir="${_prefix}/lib/pkgconfig"
+  --without-cxx-shared
+  --without-ada
+  --enable-pc-files
+  --with-sysmouse
+  --enable-overwrite
+  --with-xterm-kbs=DEL
+  --enable-sp-funcs
+  --enable-const
+  --enable-ext-colors
+  --enable-ext-mouse
+  --enable-ext-putwin
+  --enable-no-padding
+  --enable-signed-char
+  --enable-sigwinch
+  --enable-tcap-names
 )
-
-if [[ "${_variant}" != osx ]]; then
-  args+=("--enable-widec")
-fi
 
 if [[ "${_stage}" == build-install ]]; then
   if [[ "${_variant}" == x86 ]]; then
     sed -i 's/^PRG=.*$/PRG=gcc/' "${_source_root}/ncurses/ncurses/ncurses/base/MKlib_gen.sh"
   fi &&
-  mkdir -p "${_export_build_root}" &&
-  cd "${_export_build_root}" &&
+  mkdir -p "${_export_build_root}/narrowc" &&
+  cd "${_export_build_root}/narrowc" &&
   "${_source_root}/ncurses/ncurses/configure" \
     --prefix="${_prefix}" \
+    --without-progs \
     "${args[@]}" &&
-  make -j$(nproc) ${_make_trace_opt} |& tee make.log &&
-  make install ${_make_trace_opt} |& tee make.install.log &&
-  make install DESTDIR="${_export_install_root}" &&
+  make -j$(nproc) ${_make_trace_opt} libs |& tee make.log &&
+  make ${_make_trace_opt} install.libs |& tee make.install.log &&
+  make install.libs DESTDIR="${_export_install_root}" &&
+  mkdir -p "${_export_build_root}/widec" &&
+  cd "${_export_build_root}/widec" &&
+  "${_source_root}/ncurses/ncurses/configure" \
+    --prefix="${_prefix}" \
+    --enable-widec \
+    "${args[@]}" &&
+  make -j$(nproc) ${_make_trace_opt} libs |& tee make.log &&
+  make -j$(nproc) ${_make_trace_opt} -C progs |& tee -a make.log &&
+  make ${_make_trace_opt} install.{libs,progs,data,includes,man} |& \
+    tee make.install.log &&
+  make install.{libs,progs,data,includes,man} DESTDIR="${_export_install_root}" &&
+  for root in "" "${_export_install_root}"; do
+    for dir in ncurses ncursesw; do
+      target="${root}${_prefix}/include/${dir}"
+      mkdir -p "${target}" &&
+      for header in "${_export_install_root}/${_prefix}/include"/*.h; do
+        ln -sf "../$(basename "${header}")" "${target}"
+      done
+    done
+  done &&
   cd "${_source_root}/ncurses/ncurses" &&
   git checkout . &&
   git clean -f -f -d .
