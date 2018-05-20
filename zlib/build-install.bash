69331ea00c708ffaeb6e63b8f43ec2df67607525
Kan Li <likan999@users.noreply.github.com>
2018-05-20 16:02:31 -0700

    Fix zlib BUILD rules for dependent repositories and add test cases for it.
    Summary:

--- zlib/build-install.bash
+++ zlib/build-install.bash
@@ -4,6 +4,7 @@ if [[ "${_stage}" == build-install ]]; then
   cd "${_source_root}/zlib/zlib" &&
   ./configure --prefix="${_prefix}" --static &&
   make -j"${_nproc}" ${_make_trace_opt} |& tee make.log &&
+  make test -j"${_nproc}" ${_make_trace_opt} |& tee make.test.log &&
   make install ${_make_trace_opt} |& tee make.install.log &&
   make install DESTDIR="${_export_install_root}" &&
   cp -a . "${_export_build_root}"
