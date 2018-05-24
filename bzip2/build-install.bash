5268331ce4b234cd178a05bd431fdb20d7cdaa8a
Kan Li <likan999@users.noreply.github.com>
2018-05-23 21:43:25 -0700

    Fix bzip2 BUILD rules for dependent repositories.
    Summary:

--- bzip2/build-install.bash
+++ bzip2/build-install.bash
@@ -3,6 +3,7 @@
 if [[ "${_stage}" == build-install ]]; then
   cd "${_source_root}/bzip2/bzip2" &&
   make -j"${_nproc}" ${_make_trace_opt} |& tee make.log &&
+  make test -j"${_nproc}" ${_make_trace_opt} |& tee make.test.log &&
   make install ${_make_trace_opt} PREFIX="${_prefix}" |& tee make.install.log &&
   make install PREFIX="${_export_install_root}" &&
   cp -a . "${_export_build_root}"
